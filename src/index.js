1b1b9098341679c3e114bb542981cb49f9cbaa17
Pavlos Karalis <52301933+pavloskaralis@users.noreply.github.com>
2025-09-30 11:44:12 -0400

    .

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ const express = require("express")
 const morgan = require("morgan")
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
-const authController = require("./controllers/authController")
-const authMiddleware = require("./middlewares/authMiddleware")
 const errorMiddleware = require("./middlewares/errorMiddleware")
 
 const app = express()
@@ -16,7 +14,6 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(morgan("dev"))
 
-app.use("/auth", authController)
 app.use(errorMiddleware)
 
 app.listen(3001, () => console.log("Server running on port 3001"))
